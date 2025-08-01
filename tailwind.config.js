313e993082fe97178a02fa9376d4812c7ffb456f
Franky <frankyan2001@gmail.com>
2025-08-01 11:49:16 -0400

    Configurando Tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'pulse': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
+    },
   },
   plugins: [],
 }
-
