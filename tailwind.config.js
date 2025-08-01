854248bf32653da3e8e0fd9fedc9e0f57478564b
Franky <frankyan2001@gmail.com>
2025-08-01 16:35:10 -0400

    Cambiando Estructura del proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        primary: '#4f6df5',
-        secondary: '#3aa9f5',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
